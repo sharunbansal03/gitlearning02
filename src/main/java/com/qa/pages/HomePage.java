20a4fd709f4b6ee55dbd274aa6f1b89700cede63
sharunbansal04 <sharun.bansal00@gmail.com>
2020-12-02 19:01:50 +0530

    checking push with account name

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -2,6 +2,6 @@ package com.qa.pages;
 
 public class HomePage {
 	public void firstmethod() {
-		System.out.println("first method added by 04");
+		System.out.println("first method added by 04"); //comment added by 03
 	}
 }
